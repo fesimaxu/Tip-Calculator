03aa976dd42d545acf9d835414fb6ac40970c4e4
fesimaxu <fesimaxu30@gmail.com>
2022-04-16 21:47:58 +0100

    second commit

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
+let billBtn = document.getElementById('btn')
 
- function calculate(){
+billBtn.addEventListener("Click", function(){
      let billamount = document.getElementById("billamount").value;
+     billamount = ""
      console.log(billamount)
-     let billpercentage = document.getElementsById("btn").value;
-     console.log(billpercentage)
+
+     let billpercentage = document.getElementById("btn").value;
+
+
      let nPeople = document.getElementById("nPeople").value;
+     nPeople = ""
+     console.log(nPeople)
 
-     if(billamount === "" || billpercentage === 0){
-         document.write("Please input the value ");
-    }
 
-    if(nPeople === "" || nPeople <= 0){
-        document.write("Can't be zero or empty");
-    }
     let percentageAmount = (billamount * nPeople)/billpercentage;
     console.log(percentageAmount)
     
     let total = parseInt(billamount) + parseInt(tipAmount);
     console.log(total)
 
-    document.getElementById("tipAmount").value = percentageAmount;
+    document.getElementById("tipAmount").innerHTML = percentageAmount;
     document.getElementById("totalBill").innerHTML = total;
+})
 
-}
 
-document.getElementsByClassName("case-b").onclick = function(){
-    calculate();
-}
